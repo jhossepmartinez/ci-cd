ddc330545695a97c859fe18a6c6292a3bd1c0209
jhossepmartinez <jhossepmv@gmail.com>
2024-12-20 20:05:21 -0300

    remove something

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
