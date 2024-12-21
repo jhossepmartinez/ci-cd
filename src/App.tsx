b4d3d9f25bf070afc2134ac5bec5e7141c26ee89
jhossepmartinez <jhossepmv@gmail.com>
2024-12-21 12:53:51 -0300

    sera

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
         <a href="https://react.dev" target="_blank">
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
         </button>
