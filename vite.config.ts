dea0e2bb6347155106c92f0a9dead4add6453ca9
jhossepmartinez <jhossepmv@gmail.com>
2024-12-27 16:47:16 -0300

    enable port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/ci-cd",
   plugins: [react()],
+    server: {
+        port: 5173,
+        host: true,
+        strictPort: true,
+    }
 })
